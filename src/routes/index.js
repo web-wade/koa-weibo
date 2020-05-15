e4bc4fa760c6e221757b1ea1822e18c1967955be
wade <z756059388@qq.com>
2020-05-15 17:05:06 +0800

    first commit

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,19 +1,35 @@
-const router = require('koa-router')()
+const router = require("koa-router")();
 
-router.get('/', async (ctx, next) => {
-  await ctx.render('index', {
-    title: 'Hello Koa 2!'
-  })
-})
+router.get("/", async (ctx, next) => {
+  await ctx.render("index", {
+    title: "Hello Koa 2!",
+  });
+});
 
-router.get('/string', async (ctx, next) => {
-  ctx.body = 'koa2 string'
-})
+router.get("/string", async (ctx, next) => {
+  ctx.body = "koa2 string";
+});
 
-router.get('/json', async (ctx, next) => {
+router.get("/json", async (ctx, next) => {
   ctx.body = {
-    title: 'koa2 json'
-  }
-})
+    title: "koa2 json",
+  };
+});
 
-module.exports = router
+router.get("/profile/:usename", async (ctx, next) => {
+  const { usename } = ctx.params;
+  ctx.body = {
+    title: "koa2 json",
+    usename,
+  };
+});
+
+router.post("/login", async (ctx, next) => {
+  const { useName, password } = ctx.request.body;
+  ctx.body = {
+    useName,
+    password,
+  };
+});
+
+module.exports = router;
